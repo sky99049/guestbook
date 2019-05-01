85be1fb0e7eeb2ee11957b6380be0e3aead2003e
Dino Wei <dreamer@dcsh.tp.edu.tw>
2019-05-01 16:48:13 +0800

    views.py 的註解

--- message/views.py
+++ message/views.py
@@ -7,15 +7,16 @@ from .models import *
 class MessageList(ListView):
     model = Message
     ordering = ['-id']  # 依 id 欄位由大至小排序
+    # 未指定 template_name 屬性，預設使用 message/message_list.html
 
 # 檢視留言
 class MessageDetail(DetailView):
     model = Message
+    # 未指定 template_name 屬性，預設使用 message/message_detail.html
 
 # 新增留言
 class MessageCreate(CreateView):
     model = Message
-    fields = '__all__'
-    success_url = '/message/'
-
-
+    fields = '__all__'          # 顯示 *所有* 欄位
+    success_url = '/message/'   # 新增成功後，導向留言列表頁面
+    # 未指定 template_name 屬性，預設使用 message/message_form.html
